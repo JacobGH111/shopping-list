3591a696ddef01d00d5dd32c2720e9eb4ae286b2
Alex Seidman <jacobgheller@gmail.com>
2017-08-26 18:07:17 -0400

    more work

--- src/main/java/ShoppingList/main.java
+++ src/main/java/ShoppingList/main.java
@@ -1,5 +1,6 @@
 package ShoppingList;
 
+import com.google.gson.Gson;
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
@@ -7,20 +8,39 @@ import com.j256.ormlite.logger.LocalLog;
 import com.j256.ormlite.logger.Log;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
+import spark.Request;
+import spark.Response;
+import spark.ResponseTransformer;
 import spark.Spark;
 
 import java.io.FileReader;
 import java.sql.SQLException;
 import java.util.List;
+
+import static ShoppingList.JsonUtil.json;
 import static spark.Spark.*;
+
+class JsonUtil {
+
+    public static String toJson(Object object) {
+        return new Gson().toJson(object);
+    }
+
+    public static ResponseTransformer json() {
+        return JsonUtil::toJson;
+    }
+}
 
 /**
  * Created by jacob on 8/22/2017.
  */
 public class main {
+    static Gson gson = new Gson();
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         Class.forName("org.h2.Driver");
         String databaseUrl = "jdbc:h2:mem:account";
+//        String databaseUrl = "jdbc:h2:test";
+
         ConnectionSource connectionSource = new JdbcConnectionSource(databaseUrl);
 
         Dao<Store, String> storeDao = DaoManager.createDao(connectionSource, Store.class);
@@ -55,12 +75,28 @@ public class main {
 
         port(8080);
         Spark.staticFileLocation("/public");
-        get("/", (req, res) ->{
+        get("/", (req, res) -> {
             main m = new main();
 //            String r = m.getResource("../resources/static/home.html").getFile();
             String file = m.getClass().getClassLoader().getResource("./static/home.html").toString();
             FileReader reader = new FileReader(file);
             return reader.toString();
+        });
+
+        after((request, response) -> response.type("application/json"));
+        path("/stores", () -> {
+
+            get("/", (request, response) -> {
+                        return storeDao.queryForAll();
+                    },
+                    json()
+            );
+            post("/", (req, res) -> {
+                        Store newStore = gson.fromJson(req.body(), Store.class);
+                        storeDao.create(newStore);
+                        return "{\"success\": \"yes\"}";
+                    }
+            );
         });
 
     }
